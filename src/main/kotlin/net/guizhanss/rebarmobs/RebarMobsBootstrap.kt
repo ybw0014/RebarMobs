9a1ce45a58073446585d27e464e76f57b724daaa
ybw0014 <i@guizhanss.com>
2026-03-25 18:30:57 -0400

    fix: fix Soul Stealer enchantment not obtainable in enchantment table

--- src/main/kotlin/net/guizhanss/rebarmobs/RebarMobsBootstrap.kt
+++ src/main/kotlin/net/guizhanss/rebarmobs/RebarMobsBootstrap.kt
@@ -2,25 +2,34 @@ package net.guizhanss.rebarmobs
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import io.papermc.paper.registry.RegistryKey
 import io.papermc.paper.registry.data.EnchantmentRegistryEntry
 import io.papermc.paper.registry.event.RegistryEvents
 import io.papermc.paper.registry.keys.EnchantmentKeys
+import io.papermc.paper.registry.keys.tags.EnchantmentTagKeys
 import io.papermc.paper.registry.keys.tags.ItemTypeTagKeys
+import io.papermc.paper.tag.PostFlattenTagRegistrar
 import net.kyori.adventure.text.Component
 import org.bukkit.NamespacedKey
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.EquipmentSlotGroup
 
 @Suppress("UnstableApiUsage", "unused")
 class RebarMobsBootstrap : PluginBootstrap {
     override fun bootstrap(context: BootstrapContext) {
+        val soulStealerKey = NamespacedKey(PLUGIN_NAMESPACE, ENCHANTMENT_SOUL_STEALER)
+        val soulStealerTypedKey = EnchantmentKeys.create(soulStealerKey)
+
         context.lifecycleManager.registerEventHandler(
             RegistryEvents.ENCHANTMENT.compose().newHandler { event ->
                 event.registry().register(
-                    EnchantmentKeys.create(NamespacedKey(PLUGIN_NAMESPACE, "soul_stealer")),
+                    soulStealerTypedKey,
                 ) { builder ->
                     builder
                         .description(Component.translatable("$PLUGIN_NAMESPACE.enchantment.soul_stealer"))
                         .supportedItems(event.getOrCreateTag(ItemTypeTagKeys.ENCHANTABLE_MELEE_WEAPON))
+                        .primaryItems(event.getOrCreateTag(ItemTypeTagKeys.ENCHANTABLE_MELEE_WEAPON))
                         .anvilCost(3)
                         .maxLevel(5)
                         .weight(10)
@@ -30,9 +39,20 @@ class RebarMobsBootstrap : PluginBootstrap {
                 }
             },
         )
+
+        context.lifecycleManager.registerEventHandler(
+            LifecycleEvents.TAGS.postFlatten(RegistryKey.ENCHANTMENT),
+        ) { event ->
+            val registrar: PostFlattenTagRegistrar<Enchantment> = event.registrar()
+            registrar.addToTag(
+                EnchantmentTagKeys.IN_ENCHANTING_TABLE,
+                setOf(soulStealerTypedKey),
+            )
+        }
     }
 
     companion object {
         private const val PLUGIN_NAMESPACE = "rebarmobs"
+        private const val ENCHANTMENT_SOUL_STEALER = "soul_stealer"
     }
 }
